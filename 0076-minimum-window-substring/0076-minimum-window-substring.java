9a1940601eef62a8bee26bffd289ab218559cba6
varun08-dev <kumarvarun5656@gmail.com>
2026-08-26 16:41:13 +0530

    Time: 7 ms (60.85%), Space: 45.6 MB (75.52%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,47 @@
+class Solution {
+    public String minWindow(String s, String t) {
+
+    if (s.length() < t.length() || t.length() == 0) {
+        return "";
+    }
+    int startIndex =0;
+	int minLength= Integer.MAX_VALUE;
+
+	int hash[] =  new int[256];
+	for (Character character : t.toCharArray()) {
+		hash[character]++;
+	}
+
+
+    int left=0;
+    int right=0;
+    int count=0;
+    while(right<s.length()){
+        if(hash[s.charAt(right)]>0){
+            count++;
+        }
+        hash[s.charAt(right)]--;
+
+        while(count==t.length()){
+            if(right-left+1<minLength){
+                minLength=right-left+1;
+                startIndex=left;
+            }
+
+            hash[s.charAt(left)]++;
+            if(hash[s.charAt(left)]>0){
+                count--;
+            }
+            left++;
+        }
+        right++;
+    }
+		
+	if(minLength==Integer.MAX_VALUE){
+        return "";
+    }
+    String result = s.substring(startIndex, startIndex +minLength);
+    return result;
+    }
+    
+}
